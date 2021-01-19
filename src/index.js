682471e1677277b0a9e0be3d43a796fedd08614e
jeel-a-simformsolutions <jeel.a@simformsolutions.com>
2021-01-19 12:38:08 +0530

    add file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Cards';
+import Sdata from './Sdata';
+import "./index.css";
+
+console.log(Sdata[0].sname);
 
 
 
 ReactDOM.render(
   <>
-    <Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
-      title="A Netflix Orignal Series"
-      sname="DARK"
-      link="https://www.netflix.com/in/title/80990668?source=35"
+
+    <h1 className="heading_style"> List of top 5 Series in 2020</h1>
+    <Card
+      sname={Sdata[0].sname}
+      imgsrc={Sdata[0].imgsrc}
+      title={Sdata[0].title}
+      link={Sdata[0].link}
     />
-    <Card imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
-      title="A Netflix Orignal Series"
-      sname="extra curricular"
-      link="https://www.netflix.com/in/title/80990668?source=35" />
-    <Card imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
-      title="A Netflix Orignal Series"
-      sname="Stranger Things"
-      link="https://www.netflix.com/in/title/80990668?source=35" />
+    <Card
+      sname={Sdata[1].sname}
+      imgsrc={Sdata[1].imgsrc}
+      title={Sdata[1].title}
+      link={Sdata[1].link}
+    />
+    <Card
+      sname={Sdata[2].sname}
+      imgsrc={Sdata[2].imgsrc}
+      title={Sdata[2].title}
+      link={Sdata[2].link}
+    />
+    <Card
+      sname={Sdata[3].sname}
+      imgsrc={Sdata[3].imgsrc}
+      title={Sdata[3].title}
+      link={Sdata[3].link}
+    />
+    <Card
+      sname={Sdata[4].sname}
+      imgsrc={Sdata[4].imgsrc}
+      title={Sdata[4].title}
+      link={Sdata[4].link}
+    />
   </>,
   document.getElementById("root")
 );
