0b67a882a258bd238387ba5c48e82b3d50a36d92
jeel-a-simformsolutions <jeel.a@simformsolutions.com>
2021-01-20 10:03:47 +0530

    add file

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Images from './Images';
+import Heading from './Headings';
 
 function Card(props) {
-    //console.log(props)
+    //console.log(props);
     return (
         <>
             <div className="cards">
                 <div className="card ">
-                    <img src={props.imgsrc} alt="mypic" className="card_img" />
+                    <Images imgsrc={props.imgsrc} />
                     <div className="card_info">
                         <span className="card_category">{props.title}  </span>
-                        <h3 className="card_title"> {props.sname} </h3>
+                        <Heading sname={props.sname} />
                         <a href={props.link} target="_blank">
                             <button> watch Now </button>
                         </a>
--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './Cards';
-import Sdata from './Sdata';
 import "./index.css";
+import App from './App';
 
-//console.log(Sdata[0].sname);
 
-//function ncard(val) {
-//console.log(val);
-
-//);
-
-//}
 
 ReactDOM.render(
-  <>
-
-    <h1 className="heading_style"> List of top 5 Series in 2020</h1>
-
-    {Sdata.map((val) => {
-      return (
-        <Card
-          key={val.id}
-          sname={val.sname}
-          imgsrc={val.imgsrc}
-          title={val.title}
-          link={val.link}
-        />
-      );
-    })}
-  </>,
+  <App />,
   document.getElementById("root")
 
 );
